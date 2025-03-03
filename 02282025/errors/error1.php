3cf2235af7df8f894dcb48154fae8f808032ecb3
kenfutamata <kenfutamata123@gmail.com>
2025-03-03 09:07:34 +0800

    Final Database file

--- 02282025/errors/error1.php
+++ 02282025/errors/error1.php
@@ -1,10 +1,11 @@
 <?php
 
 require_once "../pdo.php"; 
+
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
 $stmt = $pdo->prepare("SELECT * FROM users where user_id = :xyz");
-$stmt->execute(array(":xyz" => $_GET['user_id']));
+$stmt->execute(array(":pizza" => $_GET['user_id']));
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 if ( $row === false ) {
     echo("<p>user_id not found</p>\n");
--- 02282025/errors/error2.php
+++ 02282025/errors/error2.php
@@ -1,15 +1,16 @@
 <?php
 
 require_once "../pdo.php"; 
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $stmt = $pdo->prepare("SELECT * FROM users where user_id = :xyz");
-$stmt->execute(array(":xyz" => $_GET['user_id']));
+$stmt->execute(array(":pizza" => $_GET['user_id']));
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 if ( $row === false ) {
     echo("<p>user_id not found</p>\n");
 } else {
     echo("<p>user_id found</p>\n");
 }
+
 
 ?>
